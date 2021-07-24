ddf43632ae5244d0e18d86a955aed2f6f9b573ca
Sergei Melnichuk <serhii.melnichuk@gmail.com>
2021-07-24 16:57:44 +0300

    restore first practice

--- js/script.js
+++ js/script.js
@@ -1,93 +1,40 @@
-"use strict";
-
-<<<<<<< HEAD
-let num = 50;
-
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
-
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num <= 55);
-
-for (let i = 1; i < 8; i++) {
-    if (i === 6) {
-        continue;
+/* Задание на урок:
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
     }
-    console.log(i);
-}
-=======
-// let a = 10;
-// console.log(a);
+Проверить, чтобы все работало без ошибок в консоли */
 
-// let number = 5;
-// const leftBorderWith = 1;
+'use strict';
 
-// number = 10;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// console.log(number);
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// const obj = {
-//     a: 50
-// };
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-// obj.a = 10;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// console.log(obj);
-
-
-// const obj = {
-//     name: 'John',
-//     age: 25,
-//     isMarried: false
-// };
-
-// console.log(obj.name);
-// console.log(obj["name"]);
-
-// let arr = ['plum.png', 'orange.jpg', 'apple=.bmp'];
-
-// console.log(arr[0]);
-
-// alert('Hello');
-
-// const result = confirm('Are you here?');
-
-// console.log(result);
-
-// const answer = +prompt('Are you 18 years old?', 'Yes');
-
-// console.log(answer + 5);
-
-const answers = [];
-
-// answers[0] = prompt('Как вас зовут?', '');
-// answers[1] = prompt('Какая у вас фамилия?', '');
-// answers[2] = prompt('Сколько вам лет?', '');
-
-console.log(typeof (answers));
-
-console.log(4 + " - object");
-
-let incr = 10,
-    decr = 10;
-
-// ++incr;
-// --decr;
-
-console.log(++incr);
-console.log(--decr);
-
-console.log(5%4);
-
-console.log(2*4 === 8);
-
-const isChecked = true,
-      isClose = false;
-
-console.log(isChecked && isClose);
->>>>>>> parent of 85ed72c... first practice
+console.log(personalMovieDB);
