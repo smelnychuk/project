c9e34e772d24da46142e41944a6399ee9d190cd6
Sergei Melnichuk <serhii.melnichuk@gmail.com>
2021-07-24 13:19:13 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// let a = 10;
+// console.log(a);
+
+// let number = 5;
+// const leftBorderWith = 1;
+
+// number = 10;
+
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+
+// const obj = {
+//     name: 'John',
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple=.bmp'];
+
+// console.log(arr[0]);
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+
+// console.log(result);
+
+// const answer = +prompt('Are you 18 years old?', 'Yes');
+
+// console.log(answer + 5);
+
+const answers = [];
+
+// answers[0] = prompt('Как вас зовут?', '');
+// answers[1] = prompt('Какая у вас фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+console.log(typeof (answers));
+
+console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%4);
+
+console.log(2*4 === 8);
+
+const isChecked = true,
+      isClose = false;
+
+console.log(isChecked && isClose);
