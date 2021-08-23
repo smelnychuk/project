4b7fc0533b727cef62463713ec7945c1f00543c2
Sergei Melnichuk <serhii.melnichuk@gmail.com>
2021-08-23 14:02:33 +0300

    Methods and properties of lines and numbers

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
 'use strict';
 
+const str = 'test';
+const arr = [1, 2, 4]
+
+console.log(arr.length);
+
+console.log(str.toUpperCase());
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('q'));
+
+const logg = 'Hello world';
+
+// console.log(logg.slice(-5, -1));
+
+// console.log(logg.substring(-6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+// console.log(parseInt(test));
+console.log(parseFloat(test));
+
+
+
