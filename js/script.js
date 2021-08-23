f6d6ee951629cac7340041f0fc1f8663475a1dac
Sergei Melnichuk <serhii.melnichuk@gmail.com>
2021-08-23 18:54:27 +0300

    Objects

--- js/script.js
+++ js/script.js
@@ -1,26 +1,42 @@
 'use strict';
 
-function first() {
-    // Do something
-    setTimeout(function() {
-        console.log(1);
-    }, 500);
+const options = {
+    name: 'test',
+    width: 1024,
+    heigh: 1024,
+    colours: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log("Test");
+    }
+};
+
+options.makeTest();
+
+const {border, bg} = options.colours;
+console.log(border);
+
+console.log(Object.keys(options).length);
+
+// console.log(options['colours']['border']);
+
+// delete options.name;
+
+// console.log(options);
+
+let counter = 0;
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]){
+            console.log(`Cвойство ${key} имеет значение ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Cвойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }   
 }
 
-function second() {
-    console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
-
-function done() {
-    console.log('Я прошел этот урок!');
-}
-
-learnJS('JavaScript', done);
+console.log(counter);
