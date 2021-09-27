3800467e32815eff053ca44f854d3a1b59e42915
Sergei Melnichuk <serhii.melnichuk@gmail.com>
2021-09-27 14:06:34 +0300

    Fifth practice

--- js/script.js
+++ js/script.js
@@ -1,23 +1,50 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
 'use strict';
 
-function hello() {
-    console.log('Hello World');
-    debugger;
-}
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
 
-hello();
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre = poster.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list');
 
-function hi() {
-    console.log('Say hi!');
-}
+adv.forEach(item => {
+    item.remove();
+});
 
-hi();
+genre.textContent = 'драма';
 
-const arr = [1, 14, 4, 30, 54],
-      sorted = arr.sort(compareNum);
+poster.style.backgroundImage = 'url("/img/bg.jpg")';
 
-function compareNum(a, b) {
-    return a - b;
-}
+movieList.innerHTML = "";
 
-console.log(sorted);
+movieDB.movies.sort();
+
+movieDB.movies.forEach ((item, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}. ${item}
+            <div class="delete"></div>
+        </li>
+    `;
+});
